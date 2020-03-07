d876f71126c8ba2f59f761307d9cc96ada0cc9f0
abhinav <ambhinav@yahoo.com>
2020-03-07 14:24:01 +0800

    added dummy users

---
+++ back-end/personaliser/users.js
@@ -0,0 +1,18 @@
+const users = [
+    {
+        name: "Ali",
+        age: 80
+    },
+    {
+        name: "Raja",
+        age: 40
+    },
+    {
+        name: "Lee",
+        age: 28
+    },
+    {
+        name: "John",
+        age: 20
+    }
+]
