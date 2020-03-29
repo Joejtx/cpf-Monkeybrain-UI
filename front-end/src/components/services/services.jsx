5518d8976d3ff16402493e8d1c1a18cb8bc94b72
Teddy <jteddysanjaya@gmail.com>
2020-03-29 15:41:32 +0800

    icons update

--- front-end/src/components/services/services.jsx
+++ front-end/src/components/services/services.jsx
@@ -3,11 +3,25 @@ import { Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { ServiceCard } from '../card/card';
 import Grid from '@material-ui/core/Grid';
-import houseLogo from '../../assets/cpf-svg/architecture-and-city.svg';
-import hospitalLogo from '../../assets/cpf-svg/hospital.svg';
-import gradHatLogo from '../../assets/cpf-svg/graduation-hat.svg';
-import calcLogo from '../../assets/cpf-svg/calculator.svg';
 import Container from '@material-ui/core/Container';
+import claimMedisave from '../../assets/cpf-svg/002-medicine.svg';
+import claimEldershield from '../../assets/cpf-svg/005-man.svg';
+import privateMedicalInsurance from '../../assets/cpf-svg/004-report.svg';
+import checkMedishield from '../../assets/cpf-svg/011-shield.svg';
+import applyBTO from '../../assets/cpf-svg/013-clipboard.svg';
+import applyHDB from '../../assets/cpf-svg/010-house.svg';
+import privateProperty from '../../assets/cpf-svg/012-building.svg';
+import houseInsurance from '../../assets/cpf-svg/011-shield.svg';
+import withdrawCPF from '../../assets/cpf-svg/008-withdraw.svg';
+import topupCPF from '../../assets/cpf-svg/007-device.svg';
+import saveForRetirement from '../../assets/cpf-svg/005-man.svg';
+import silverSupport from '../../assets/cpf-svg/005-man.svg';
+import payForChildren from '../../assets/cpf-svg/014-student.svg';
+import invest from '../../assets/cpf-svg/001-hand.svg';
+import selfEmployed from '../../assets/cpf-svg/003-injection.svg';
+import employeesCPF from '../../assets/cpf-svg/009-encourage.svg';
+import nominationCPF from '../../assets/cpf-svg/013-clipboard.svg';
+import calculate from '../../assets/cpf-svg/015-percentage.svg';
 
 const useStyles = makeStyles(theme => ({
     title: {
@@ -21,11 +35,26 @@ const useStyles = makeStyles(theme => ({
 })); 
 
 const logos = [
-    ["housing", houseLogo],
-    ["hospital", hospitalLogo],
-    ["education", gradHatLogo],
-    ["calculator", calcLogo]
+    ["Claim Medisave", claimMedisave],
+    ["Claim Eldershield", claimEldershield],
+    ["Apply Private Medical Insurance", privateMedicalInsurance],
+    ["Check Medishield", checkMedishield],
+    ["Apply BTO", applyBTO],
+    ["Apply HDB", applyHDB],
+    ["Private Property", privateProperty],
+    ["House Insurance", houseInsurance],
+    ["Withdraw CPF Fund", withdrawCPF],
+    ["Top Up CPF", topupCPF],
+    ["Save for Retirement", saveForRetirement],
+    ["Pay for Your Children", payForChildren],
+    ["Invest Your Money", invest],
+    ["Self Employed", selfEmployed],
+    ["Silver Support", silverSupport],
+    ["Contribute CPF for Employees", employeesCPF],
+    ["CPF Nomination", nominationCPF],
+    ["CPF Calculator", calculate]
 ]
+
 
 export default function CpfServices(props) {
     const classes = useStyles();
